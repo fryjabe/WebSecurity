8f98a44562958345e8759c944e98eb0189c39f1d
fryjabe <fryszczynn@gmail.com>
2018-11-16 15:21:28 +0100

    commit with weird stuff (i cannot checkout to other branch)

--- models/post.js
+++
@@ -1,8 +0,0 @@
-const mongoose= require('mongoose');
-
-const postSchema = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    content: String
-});
-
-module.exports= mongoose.model('Post', postSchema);
